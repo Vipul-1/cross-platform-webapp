661f08ffdaf9af17f2995851b5f667021e9080ae
Vipul Poojara <vipul.poojara@gmail.com>
2016-12-29 22:22:24 +0530

    Add files via upload
    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using System.Collections;
+
+namespace cross_platform_webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = RuntimeInformation.OSDescription + " " + RuntimeInformation.OSArchitecture.ToString();
+            ViewData["Framework"] = RuntimeInformation.FrameworkDescription;
+            ViewData["ProcessArchitecture"] = RuntimeInformation.ProcessArchitecture;
+            ViewBag.EnviornmentVariables = System.Environment.GetEnvironmentVariables();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
